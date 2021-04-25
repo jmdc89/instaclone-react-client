fa9e5af14a466f206e0a5a40764be1761e369a94
Jose Manuel <jmdcarq89@gmail.com>
2021-04-25 18:30:37 +0200

    Regform Form

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -10,7 +10,51 @@ export default function RegisterForm(props) {
       <h2 className="register-form-title">
         Regístrate para ver fotos y vídeos de tus amigos.
       </h2>
-      <Form></Form>
+      <Form className="register-form">
+        <Form.Input
+          type="text"
+          placeholder="Nombre y apellidos"
+          name="name"
+          //   value={formik.values.name}
+          //   onChange={formik.handleChange}
+          //   error={formik.errors.name && true}
+        />
+        <Form.Input
+          type="text"
+          placeholder="Nombre de usuario"
+          name="username"
+          //   value={formik.values.username}
+          //   onChange={formik.handleChange}
+          //   error={formik.errors.username && true}
+        />
+        <Form.Input
+          type="text"
+          placeholder="Correo electronico"
+          name="email"
+          //   value={formik.values.email}
+          //   onChange={formik.handleChange}
+          //   error={formik.errors.email && true}
+        />
+        <Form.Input
+          type="password"
+          placeholder="Contraseña"
+          name="password"
+          //   value={formik.values.password}
+          //   onChange={formik.handleChange}
+          //   error={formik.errors.password && true}
+        />
+        <Form.Input
+          type="password"
+          placeholder="Repetir contraseña"
+          name="repeatPassword"
+          //   value={formik.values.repeatPassword}
+          //   onChange={formik.handleChange}
+          //   error={formik.errors.repeatPassword && true}
+        />
+        <Button type="submit" className="btn-submit">
+          Registrarse
+        </Button>
+      </Form>
     </>
   );
 }
