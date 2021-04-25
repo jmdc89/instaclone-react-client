61bb88626673ef49969f0726d21799632473bca6
Jose Manuel <jmdcarq89@gmail.com>
2021-04-25 19:33:51 +0200

    Formik data

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,26 +1,37 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
 import "./RegisterForm.scss";
 
 export default function RegisterForm(props) {
   const { setShowLogin } = props;
 
-  const onSubmit = () => {
-    console.log("Formulario enviado");
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    validationSchema: null,
+    onSubmit: (formValue) => {
+      console.log(formValue);
+    },
+  });
 
   return (
     <>
       <h2 className="register-form-title">
         Regístrate para ver fotos y vídeos de tus amigos.
       </h2>
-      <Form className="register-form" onSubmit={onSubmit}>
+      <Form className="register-form" onSubmit={formik.handleSubmit}>
         <Form.Input
           type="text"
           placeholder="Nombre y apellidos"
           name="name"
           //   value={formik.values.name}
-          //   onChange={formik.handleChange}
+          onChange={formik.handleChange}
           //   error={formik.errors.name && true}
         />
         <Form.Input
@@ -28,7 +39,7 @@ export default function RegisterForm(props) {
           placeholder="Nombre de usuario"
           name="username"
           //   value={formik.values.username}
-          //   onChange={formik.handleChange}
+          onChange={formik.handleChange}
           //   error={formik.errors.username && true}
         />
         <Form.Input
@@ -36,7 +47,7 @@ export default function RegisterForm(props) {
           placeholder="Correo electronico"
           name="email"
           //   value={formik.values.email}
-          //   onChange={formik.handleChange}
+          onChange={formik.handleChange}
           //   error={formik.errors.email && true}
         />
         <Form.Input
@@ -44,7 +55,7 @@ export default function RegisterForm(props) {
           placeholder="Contraseña"
           name="password"
           //   value={formik.values.password}
-          //   onChange={formik.handleChange}
+          onChange={formik.handleChange}
           //   error={formik.errors.password && true}
         />
         <Form.Input
@@ -52,7 +63,7 @@ export default function RegisterForm(props) {
           placeholder="Repetir contraseña"
           name="repeatPassword"
           //   value={formik.values.repeatPassword}
-          //   onChange={formik.handleChange}
+          onChange={formik.handleChange}
           //   error={formik.errors.repeatPassword && true}
         />
         <Button type="submit" className="btn-submit">
