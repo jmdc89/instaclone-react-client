e75059845bfdc6abe3fbdd8b0f2588529dab3d6f
Jose Manuel <jmdcarq89@gmail.com>
2021-04-25 20:08:32 +0200

    toastify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
