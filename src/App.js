dd1e2dc6637f2be20562186eb298acb2c7b90a7c
Jose Manuel <jmdcarq89@gmail.com>
2021-04-26 17:44:07 +0200

    Home.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import client from "./config/apollo";
 import Auth from "./pages/Auth";
 import { getToken, decodeToken, removeToken } from "./utils/token";
 import AuthContext from "./context/AuthContext";
+import Home from "./pages/Home";
 
 export default function App() {
   const [auth, setAuth] = useState(undefined);
@@ -41,7 +42,7 @@ export default function App() {
   return (
     <ApolloProvider client={client}>
       <AuthContext.Provider value={authData}>
-        {!auth ? <Auth /> : <h1>Estas logueado</h1>}
+        {!auth ? <Auth /> : <Home />}
         <ToastContainer
           position="top-right"
           autoClose={5000}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import useAuth from "../../hooks/useAuth";
+// import { Grid } from "semantic-ui-react";
+// import Feed from "../../components/Home/Feed";
+// import UsersNotFolloweds from "../../components/Home/UsersNotFolloweds";
+// import "./Home.scss";
+
+export default function Home() {
+  const auth = useAuth();
+  console.log(auth);
+
+  return (
+    <h1>Estamos en Home</h1>
+    // <Grid className="home">
+    //   <Grid.Column className="home__left" width={11}>
+    //     <Feed />
+    //   </Grid.Column>
+    //   <Grid.Column className="home__right" width={5}>
+    //     <UsersNotFolloweds />
+    //   </Grid.Column>
+    // </Grid>
+  );
+}
