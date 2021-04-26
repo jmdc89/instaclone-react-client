13a65363af9b4f06938dbb7dac7b03c92743eccc
Jose Manuel <jmdcarq89@gmail.com>
2021-04-26 17:33:22 +0200

    api context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { ApolloProvider } from "@apollo/client";
 import { ToastContainer } from "react-toastify";
 import client from "./config/apollo";
 import Auth from "./pages/Auth";
 import { getToken, decodeToken, removeToken } from "./utils/token";
+import AuthContext from "./context/AuthContext";
 
 export default function App() {
   const [auth, setAuth] = useState(undefined);
@@ -17,20 +18,42 @@ export default function App() {
     }
   }, []);
 
+  const logout = () => {
+    removeToken();
+    setAuth(null);
+  };
+
+  const setUser = (user) => {
+    setAuth(user);
+  };
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      logout,
+      setUser,
+    }),
+    [auth]
+  );
+
+  if (auth === undefined) return null;
+
   return (
     <ApolloProvider client={client}>
-      {!auth ? <Auth /> : <h1>Estas logueado</h1>}
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+      <AuthContext.Provider value={authData}>
+        {!auth ? <Auth /> : <h1>Estas logueado</h1>}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </AuthContext.Provider>
     </ApolloProvider>
   );
 }
