bb00365d8ed026751bce082c60c902b37878dd94
Jose Manuel <jmdcarq89@gmail.com>
2021-04-28 12:52:18 +0200

    RightHeader fix

--- src/components/Header/RightHeader/RightHeader.js
+++ src/components/Header/RightHeader/RightHeader.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Icon, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-// import { useQuery } from "@apollo/client";
-// import { GET_USER } from "../../../gql/user";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../../../gql/user";
 import useAuth from "../../../hooks/useAuth";
 // import ModalUpload from "../../Modal/ModalUpload";
 import ImageNoFound from "../../../assets/png/avatar.png";
@@ -11,12 +11,12 @@ import "./RightHeader.scss";
 export default function RightHeader() {
   //   const [showModal, setShowModal] = useState(false);
   const { auth } = useAuth();
-  //   const { data, loading, error } = useQuery(GET_USER, {
-  //     variables: { username: auth.username },
-  //   });
+  const { data, loading, error } = useQuery(GET_USER, {
+    variables: { username: auth.username },
+  });
 
-  //   if (loading || error) return null;
-  //   const { getUser } = data;
+  if (loading || error) return null;
+  const { getUser } = data;
 
   return (
     <>
@@ -26,11 +26,8 @@ export default function RightHeader() {
         </Link>
         {/* <Icon name="plus" onClick={() => setShowModal(true)} /> */}
         <Icon name="plus" />
-        {/* <Link to={`/${auth.username}`}>
-          <Image src={getUser.avatar ? getUser.avatar : ImageNoFound} avatar />
-        </Link> */}
         <Link to={`/${auth.username}`}>
-          <Image src={ImageNoFound} avatar />
+          <Image src={getUser.avatar ? getUser.avatar : ImageNoFound} avatar />
         </Link>
       </div>
       {/* <ModalUpload show={showModal} setShow={setShowModal} /> */}
