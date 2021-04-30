9114c6675a492dbc78c9a2112f316a3ab1ebaa35
Jose Manuel <jmdcarq89@gmail.com>
2021-04-30 22:50:39 +0200

    Descformik

--- src/components/User/DescriptionForm/DescriptionForm.js
+++ src/components/User/DescriptionForm/DescriptionForm.js
@@ -11,6 +11,28 @@ export default function DescriptionForm(props) {
   const { setShowModal, currentDescription, refetch } = props;
   const [updateUser] = useMutation(UPDATE_USER);
 
+  const formik = useFormik({
+    initialValues: {
+      description: currentDescription || "",
+    },
+    validationSchema: Yup.object({
+      description: Yup.string().required(),
+    }),
+    onSubmit: async (formData) => {
+      try {
+        await updateUser({
+          variables: {
+            input: formData,
+          },
+        });
+        refetch();
+        setShowModal(false);
+      } catch (error) {
+        toast.error("Error al actualziar tu biografía");
+      }
+    },
+  });
+
   return (
     <Form className="description-form" onSubmit={formik.handleSubmit}>
       <TextArea
